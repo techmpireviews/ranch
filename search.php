c76d024d573c379c36c559b0d14bdac63f5286f0
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-10-26 21:37:22 -0500

    Update search.php

--- search.php
+++ search.php
@@ -4,8 +4,128 @@
 	<title>Bauman Red Angus</title>
 	<link rel="icon" href="images/headlogo.png">
 </head>
+<header>
+	<p id="menu"><span id="links"><a class="a1" href="">Home </a>&nbsp;&nbsp;&nbsp;&nbsp; <a class="a1" href="/cattle.php">Cattle </a>&nbsp;&nbsp;&nbsp;&nbsp; <a class="a1" href="/horses.php">Horses </a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/contact.php">Contact Us </a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/about.php">About Us </a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/search.php"> Search</a></span><img id="menulogo" src="images/whitemenu.png"></p>
+</header>
 <body>
-<center><h1 style="font-size: 40px;">There are no results found for this search.<br><a style="text-decoration: none; position: relative; margin-left: -75px;" href="/index.php"><img style="width: 30px; height: auto;  margin-top: -5px; transform: rotate(180deg);"src="images/back.png"><b id="back" style="color: blue; position: absolute; margin-top: -1.5px; margin-left: 5px;"> Back</b></a></h1><br><br><br><br><br><br></center>
+	<center><b style="font-size: 60px; font-family: sans-serif;">Search</b></center>
+	<div class="search">
+		<span class="searchformspan">
+			<form autocomplete="off" id="searchform" action="search.php" method="post">
+				<img id="searchicon" style="width: 40px; height: auto;" src="images/searchicon.png">
+				<input style="margin-left: 5px;" placeholder="search" autocomplete="off" id="searchid" type="text" name="search">
+			</form>
+		</span>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<style type="text/css">
+	body {
+		background-image: linear-gradient(to right, #B7E2F9, #ffffff);
+		margin-top: 7%;
+	}
+	input:focus {
+		outline: none;
+	}
+
+	#searchform {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 8px;
+	}
+
+	#searchid {
+		margin-top: -5px;
+		border: solid;
+		background-color: transparent;
+		border-color: transparent;
+		border-bottom-color: #000000;
+		border-width: 1px;
+		width: 400px;
+		height: 25px;
+	}
+	#menu {
+	position: absolute;
+	top: -18px;
+	left: -1px;
+	right: 0px;
+	background-color: #000000;
+	border: solid;
+	border-color: #000000;
+    clip-path: circle(50px at 15px 14px);
+	padding: 35px;
+	transition: 0.5s;
+	z-index: 50;
+}
+
+#menu:hover {
+	position: absolute;
+	top: -18px;
+	left: -1px;
+	right: 0px;
+	border: solid;
+	border-color: #ffffff;
+	border-bottom-color: #000000;
+    clip-path: circle(100%);
+	background-color: #ffffff;
+	padding: 33px;
+	transition: 0.85s;
+	z-index: 50;
+}
+.a1 {
+	padding: 2px 7px;
+	text-decoration: none;
+	color: #000000;
+	border: solid;
+	border-color: transparent;
+	background-color: transparent;
+	border-radius: 7px;
+	font-size: 25px;
+	transition: 0.5s;
+}
+
+.a1:hover {
+	padding: 2px 7px;
+	text-decoration: none;
+	color: #ffffff;
+	border: solid;
+	border-color: #000000;
+	background-color: #000000;
+	border-radius: 15px;
+	font-size: 25px;
+	transition: 0.6s;
+}
+
+#links {
+	position: absolute;
+	margin-left: 48.5%;
+	margin-right: -50%;
+	transform: translate(-50%, -50%);
+}
+
+#menulogo {
+	width: 30px;
+	height: auto;
+	position: absolute;
+	top: 16px;
+	left: 12px;
+}
+</style>
+
+
+
+
 <?php
 $result= $_POST['search'];
 $result= strtolower($result);
@@ -20,9 +140,9 @@ header ('Location: /searchabout.php');
 	
 if ($result=="contact us")
 header ('Location: /searchcontact.php');
-	
-	if ($result=="home")
-header ('Location: /searchhome.php');
+
+echo "<center>No results for '$result'.</center>";
 ?>
 </body>
 </html>
+
