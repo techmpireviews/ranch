534b85dcd8c9b5452e5821279a78648d4a2ac497
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-10-26 23:18:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Bauman Red Angus</title>
 	<link rel="icon" href="images/headlogo.png">
 	<link rel="stylesheet" type="text/css" href="style1.css">
+	<script src="width.js"></script>
 </head>
 <header>
 	<p id="menu"><span id="links"><a class="a1" href="">Home </a>&nbsp;&nbsp;&nbsp;&nbsp; <a class="a1" href="/cattle.php">Cattle </a>&nbsp;&nbsp;&nbsp;&nbsp; <a class="a1" href="/horses.php">Horses </a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/contact.php">Contact Us </a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/about.php">About Us </a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/search.php"> Search</a></span><img id="menulogo" src="images/menu.png"></p>
